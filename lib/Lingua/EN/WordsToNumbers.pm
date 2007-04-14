fda233b2e314a0a58b3002776be1a943470e86c0
HEX <hex@cpan.org>
2007-04-14 21:14:05 +0000

    Import of HEX/Lingua-EN-WordsToNumbers-0.2 from CPAN.
    gitpan-cpan-distribution: Lingua-EN-WordsToNumbers
    gitpan-cpan-version:      0.2
    gitpan-cpan-path:         HEX/Lingua-EN-WordsToNumbers-0.2.tar.gz
    gitpan-cpan-author:       HEX
    gitpan-cpan-maturity:     released

--- lib/Lingua/EN/WordsToNumbers.pm
+++ lib/Lingua/EN/WordsToNumbers.pm
@@ -3,54 +3,78 @@ package Lingua::EN::WordsToNumbers;
 use warnings;
 use strict;
 
-use vars qw($VERSION);
+our $VERSION = 0.2;
 
-$VERSION = 0.11;
+use base qw(Exporter);
+our @EXPORT_OK = qw(words_to_numbers);
 
+use Carp;
 use Lingua::EN::Words2Nums ();
 
-sub new
-{
+sub words_to_numbers {
+  Lingua::EN::Words2Nums::words2nums(shift());
+}
+
+sub set_debug {
+  my $debug = shift;
+  
+  if (!$debug) {
+    warn 'No value given to set_debug()';
+    return;
+  }
+  
+  if ($debug =~ /^y$/i) {
+    $Lingua::EN::Words2Nums::debug = 1;
+  } elsif ($debug =~ /^n$/i) {
+    $Lingua::EN::Words2Nums::debug = 0;
+  } else {
+    carp "Unknown value '$debug' given to set_debug()";
+  }
+}
+
+sub set_billion {
+  my $billion = shift;
+
+  $Lingua::EN::Words2Nums::billion = $billion;
+
+  carp "Value of billion set to $billion."
+    if $Lingua::EN::Words2Nums::debug && $Lingua::EN::Words2Nums::debug == 1;
+}
+
+# ---------------------------------------------------------------------------
+# Curious, deprecated OO methods follow.
+# ---------------------------------------------------------------------------
+
+sub new {
   my $class = shift;
   my $self = bless {}, $class;
   
   return $self;
 }
 
-sub parse
-{
+sub parse {
   my ($self, $number) = @_;
 
   Lingua::EN::Words2Nums::words2nums($number);  
 }
 
-sub words2nums
-{
+sub words2nums {
   my ($self, $number) = @_;
 
   Lingua::EN::Words2Nums::words2nums($number);  
 }
 
-sub debug
-{
+sub debug {
   my ($self, $debug) = @_;
 
-  warn $debug;
-
-  if ($debug)
-  {
+  if ($debug) {
     $Lingua::EN::Words2Nums::debug = 1;
-  }
-  else
-  {
+  } else {
     $Lingua::EN::Words2Nums::debug = 0;
   }
-
-  warn $Lingua::EN::Words2Nums::debug;
 }
 
-sub billion
-{
+sub billion {
   my ($self, $billion) = @_;
   
   $Lingua::EN::Words2Nums::billion = $billion;
@@ -66,42 +90,73 @@ Lingua::EN::WordsToNumbers - convert numbers written in English to actual number
 
 =head1 DESCRIPTION
 
-This module is an OO wrapper for L<Lingua::EN::Words2Nums>, which is very nice, but has an unfortunate name (which doesn't help when you're searching CPAN for modules that turn words into numbers).
+This module is a tiny wrapper for L<Lingua::EN::Words2Nums>, which is very nice, but has an unfortunate name (which doesn't help when you're searching CPAN for modules that turn words into numbers).
 
 =head1 SYNOPSIS
 
-   use Lingua::EN::WordsToNumbers;
+   use Lingua::EN::WordsToNumbers qw(words_to_numbers);
    
-   my $numberizer = Lingua::EN::WordsToNumbers->new;
-   
-   print $numberizer->parse("Forty-two"); # "42"
+   print words_to_numbers("Forty-two");             # "42"
 
-   $numberizer->debug(1);                 # debug on
-   $numberizer->debug(undef);             # debug off
+   Lingua::EN::WordsToNumbers->set_debug('y');      # debug mode on
+   Lingua::EN::WordsToNumbers->set_debug('n');      # debug mode on
    
-   $numberizer->billion(10**12);          # use "English billion"
+   Lingua::EN::WordsToNumbers->set_billion(10**12); # use "English billion"
    
 =head1 METHODS
 
-=head2 C<new>
+=head2 C<words_to_numbers()>
+
+An alias for L<Lingua::EN::Words2Nums>'s C<words2nums> method. Read that 
+module's documentation to see how it works. 
+
+=head2 C<set_debug()>
+
+   Lingua::EN::WordsToNumbers->set_debug('y');
+   
+Set L<Lingua::EN::Words2Nums> debug mode. 'y' or 'Y' is on, 'n' or 'N' is off.
+Not exportable; call directly.
+   
+=head2 C<set_billion()>
+
+   Lingua::EN::WordsToNumbers->set_billion(10**12);
+
+Define the value of a billion (C<$Lingua::EN::Words2Nums::billion>). Not
+exportable; call directly.
+
+=head1 DEPRECATED METHODS
+
+You know how, every now and then, you look at some of your code from a few
+years ago and think "What the hell was I smoking?" Well, this is that code.
+The following methods are a completely pointless OO interface to this module
+and I recommend ignoring them completely. They'll go away in the next version
+of this module - say in about a year's time.
+
+=head3 C<new()>
 
    my $numberizer = Lingua::EN::WordsToNumbers->new;
 
 Gives you a new "object". Doesn't really do much.
 
-=head2 C<parse>
+=head3 C<parse()>
 
    print $numberizer->parse("Forty-two"); # "42"
 
-An alias for L<Lingua::EN::Words2Nums>'s C<words2nums> method. Read that module's documentation to see how it works. You can also call this method as C<words2nums> if you really want to.
+An alias for L<Lingua::EN::Words2Nums>'s C<words2nums> method. Read that module's documentation to see how it works.
 
-=head2 C<debug>
+=head3 C<words2nums()>
+
+   print $numberizer->words2nums("Forty-two"); # "42"
+
+Same as above. Legacy method; deprecated.
+
+=head3 C<debug()>
 
    $numberizer->debug(1);
    
 Set L<Lingua::EN::Words2Nums> debug mode. True values are on, anything else is off.
    
-=head2 C<billion>
+=head3 C<billion()>
 
    $numberizer->billion(10**12);
 
@@ -109,23 +164,19 @@ Define the value of a billion (C<$Lingua::EN::Words2Nums::billion>).
 
 =head1 AUTHOR
 
-Earle Martin <EMARTIN@cpan.org>
+Earle Martin <hex@cpan.org>
 
-=over 4 
-
-=item * L<http://purl.oclc.org/net/earlemartin/>
-
-=back
+L<http://downlode.org/>
 
 =head1 CREDITS
 
-Thanks to Joey Hess (JOEY) for writing L<Lingua::EN::Words2Nums> which does the real work.
+All credit is due to Joey Hess (JOEY) for writing L<Lingua::EN::Words2Nums> which does the actual work.
 
 This module was jointly conceived with Leon Brocard (LBROCARD) one lunchtime.
 
 =head1 LEGAL
 
-Copyright 2004 Earle Martin. All Rights Reserved.
+Copyright 2004-2007 Earle Martin. All rights reserved.
 
 This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
 
--- t/numberize.t
+++ t/numberize.t
@@ -3,15 +3,41 @@
 use warnings;
 use strict;
 
-use Test::More tests => 6;
+use Test::More tests => 10;
+use Test::Warn;
 
-require_ok("Lingua::EN::Words2Nums");
-use_ok("Lingua::EN::WordsToNumbers");
+BEGIN { use_ok('Lingua::EN::WordsToNumbers', qw(words_to_numbers)) }
+ 
+is( words_to_numbers('Forty-two'), 42, 'words_to_numbers()' );
+
+warning_is
+  { Lingua::EN::WordsToNumbers::set_debug() } 
+  'No value given to set_debug()', 
+  'set_debug() without value warns';
+  
+warning_is
+  { Lingua::EN::WordsToNumbers::set_debug('fred') } 
+  "Unknown value 'fred' given to set_debug()", 
+  'set_debug() with bogus value warns';
+
+Lingua::EN::WordsToNumbers::set_debug('y');
+
+warning_like
+  { Lingua::EN::WordsToNumbers::set_billion(10**12) }
+  qr/Value of billion set to 1000000000000/, 
+  'set_billion() in debug mode';
+
+is( words_to_numbers('One billion'), 1000000000000, 'set_billion()' );
+
+# Deprecated methods. Resetting to default values for tests...
+Lingua::EN::WordsToNumbers::set_debug('n');
+Lingua::EN::WordsToNumbers::set_billion(10**9);
 
 my $numberizer = Lingua::EN::WordsToNumbers->new;
-isa_ok($numberizer, "Lingua::EN::WordsToNumbers");
+isa_ok( $numberizer, 'Lingua::EN::WordsToNumbers' );
 
-is ($numberizer->words2nums("Forty-two"), 42, 'words2nums()');
+is ($numberizer->words2nums('Forty-two'), 42, 'words2nums()');
 is ($numberizer->parse("Forty-two"), 42, 'parse()');
+
 $numberizer->billion(10**12);
 is ($numberizer->parse("One billion"), 1000000000000, 'billion()');
